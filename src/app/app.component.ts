f125e2408327d7311438fa2825c3e219d9078a76
Natalia Repkina <pn.88@mail.ru>
2025-07-16 15:07:33 +0300

    component life cycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,75 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Lean Angular Course 2025';
-  pBlue: string = 'blue';
+  name: string = 'Название';
+  obj: any = { content: 'тут лежит что-то' };
+
+  constructor() {
+    console.log(
+      '%c PARENT: Constructor',
+      'background: #CE93D8; color: #4A148C; padding: 3px 5px; border-radius: 4px; font-weight: bold'
+    );
+
+    setTimeout(() => (this.title = 'Second attempt'), 3000);
+
+    setTimeout(() => {
+      this.obj = { ...this.obj, content: 'тут лежит что-то НОВОЕ' };
+    }, 5000);
+  }
+
+  ngOnChanges(): void {
+    console.log(
+      `%c PARENT: ngOnChanges }`,
+      'background: #BBDEFB; color: #0D47A1; padding: 3px 5px; border-radius: 4px; font-weight: bold'
+    );
+  }
+
+  ngOnInit(): void {
+    console.log(
+      '%c PARENT: ngOnInit',
+      'background: #C8E6C9; color: #1B5E20; padding: 3px 5px; border-radius: 4px; font-weight: bold'
+    );
+  }
+
+  ngDoCheck(): void {
+    console.log(
+      '%c PARENT: ngDoCheck',
+      'background: #FFF9C4; color: #F57F17; padding: 3px 5px; border-radius: 4px; font-weight: bold'
+    );
+  }
+
+  ngAfterContentInit(): void {
+    console.log(
+      '%c PARENT: ngAfterContentInit',
+      'background: #B3E5FC; color: #01579B; padding: 3px 5px; border-radius: 4px; font-weight: bold'
+    );
+  }
+
+  ngAfterContentChecked(): void {
+    console.log(
+      '%c PARENT: ngAfterContentChecked',
+      'background: #DCEDC8; color: #33691E; padding: 3px 5px; border-radius: 4px; font-weight: bold'
+    );
+  }
+
+  ngAfterViewInit(): void {
+    console.log(
+      '%c PARENT: ngAfterViewInit (Child доступен)',
+      'background: #FFCCBC; color: #BF360C; padding: 3px 5px; border-radius: 4px; font-weight: bold'
+    );
+  }
+
+  ngAfterViewChecked(): void {
+    console.log(
+      '%c PARENT: ngAfterViewChecked',
+      'background: #F8BBD0; color: #880E4F; padding: 3px 5px; border-radius: 4px; font-weight: bold'
+    );
+  }
+
+  ngOnDestroy(): void {
+    console.log(
+      '%c PARENT: ngOnDestroy',
+      'background: #CFD8DC; color: #263238; padding: 3px 5px; border-radius: 4px; font-weight: bold'
+    );
+  }
 }
--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, SimpleChange } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -6,12 +6,70 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./child.component.scss'],
 })
 export class ChildComponent {
-  @Input('childColor') color!: string;
+  @Input() title!: string;
   @Input() name!: string;
+  @Input() obj!: any;
 
-  @Output() nameChange = new EventEmitter<string>();
+  constructor() {
+    console.log(
+      '%c CHILD: Constructor',
+      'background: #E1BEE7; color: #7B1FA2; padding: 3px 5px; border-radius: 4px; border-left: 4px solid #4A148C'
+    );
+  }
 
-  changeName() {
-    this.nameChange.emit('Имя заданное в дочернем компоненте');
+  ngOnChanges(changes: SimpleChange): void {
+    console.log(
+      `%c CHILD: ngOnChanges ${JSON.stringify(changes)}`,
+      'background: #E3F2FD; color: #1565C0; padding: 3px 5px; border-radius: 4px; border-left: 4px solid #0D47A1'
+    );
+  }
+
+  ngOnInit(): void {
+    console.log(
+      '%c CHILD: ngOnInit',
+      'background: #DCEDC8; color: #388E3C; padding: 3px 5px; border-radius: 4px; border-left: 4px solid #1B5E20'
+    );
+  }
+
+  ngDoCheck(): void {
+    console.log(
+      '%c CHILD: ngDoCheck',
+      'background: #FFF8E1; color: #FFA000; padding: 3px 5px; border-radius: 4px; border-left: 4px solid #F57F17'
+    );
+  }
+
+  ngAfterContentInit(): void {
+    console.log(
+      '%c CHILD: ngAfterContentInit',
+      'background: #E1F5FE; color: #0277BD; padding: 3px 5px; border-radius: 4px; border-left: 4px solid #01579B'
+    );
+  }
+
+  ngAfterContentChecked(): void {
+    console.log(
+      '%c CHILD: ngAfterContentChecked',
+      'background: #F1F8E9; color: #689F38; padding: 3px 5px; border-radius: 4px; border-left: 4px solid #33691E'
+    );
+  }
+
+  ngAfterViewInit(): void {
+    console.log(
+      '%c CHILD: ngAfterViewInit',
+      'background: #FBE9E7; color: #E64A19; padding: 3px 5px; border-radius: 4px; border-left: 4px solid #BF360C'
+    );
+  }
+
+  ngAfterViewChecked(): void {
+    console.log(
+      '%c CHILD: ngAfterViewChecked',
+      'background: #FCE4EC; color: #C2185B; padding: 3px 5px; border-radius: 4px; border-left: 4px solid #880E4F'
+    );
+  }
+
+  ngOnDestroy(): void {
+    console.log(
+      '%c CHILD: ngOnDestroy',
+      'background: #ECEFF1; color: #455A64; padding: 3px 5px; border-radius: 4px; border-left: 4px solid #263238'
+    );
   }
 }
