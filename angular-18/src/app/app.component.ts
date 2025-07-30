138fc170ee66ddc8521520f20582d2208ddee606
Natalia Repkina <pn.88@mail.ru>
2025-07-30 15:20:08 +0300

    upd

---
+++ angular-18/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'angular-18';
+
+  someObservable = new Observable((sub)=>{
+    console.log(sub)
+  });
+}
--- src/app/shared/components/green/green.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-green',
-  templateUrl: './green.component.html',
-  styleUrl: './green.component.scss',
-})
-export class GreenComponent {}
