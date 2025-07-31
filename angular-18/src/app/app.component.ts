b32cd8f1d0c4a49740151256bcd91798147d14da
Natalia Repkina <pn.88@mail.ru>
2025-07-31 11:09:57 +0300

    lesson RxJs created operators

--- angular-18/src/app/app.component.ts
+++ angular-18/src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Observable } from 'rxjs';
+import { from, fromEvent, interval, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +9,26 @@ import { Observable } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-18';
 
-  someObservable = new Observable((sub)=>{
-    console.log(sub)
-  });
+  numberObservable$: Observable<number> = new Observable((subscriber) => {
+    subscriber.next(1);
+    subscriber.next(2);
+    subscriber.next(3);
+    subscriber.next(4);
+    subscriber.next(5);
+    subscriber.complete();
+  })
+
+  ngOnInit() {
+    this.numberObservable$.subscribe(console.log);
+
+    console.log('______________________')
+
+    of(1,2,3,4,5).subscribe(console.log);
+    from([1,2,3,4,5]).subscribe(console.log);
+    fromEvent(document, 'click').subscribe(console.log);
+    interval(1000).subscribe(console.log);
+  }
 }
